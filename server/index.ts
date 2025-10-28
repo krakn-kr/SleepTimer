96124b3fe778145d1638ec25dd74a8991d94af7e
krashokkumarnaidu@gmail.com <krashokkumarnaidu@gmail.com>
2025-10-29 01:06:52 +0530

    try 6

--- server/index.ts
+++ server/index.ts
@@ -70,8 +70,10 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen(port, () => {
-    log(`serving on port ${port}`);
-  });
+  if (process.env.NODE_ENV !== 'production') {
+    const port = parseInt(process.env.PORT || '5000', 10);
+    app.listen(port, () => {
+      log(`serving on port ${port}`);
+    });
+  }
 })();
